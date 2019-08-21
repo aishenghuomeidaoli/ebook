a97020c6c5f7c36d47e79188e75b55d0fd894b26
zhujiawei <aishenghuomeidaoli@163.com>
2019-08-22 00:24:15 +0800

    添加数据库查询

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-from db import close_db, init_db_command
+from db import close_db, init_db_command, get_db
 
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
@@ -9,4 +9,6 @@ app.cli.add_command(init_db_command)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    db = get_db()
+    rows = db.execute("select * from books")
+    return render_template('index.html', rows=rows)
