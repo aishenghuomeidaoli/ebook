911a48868b529f2d3e7466eb6fca197fd15615df
zhujiawei <aishenghuomeidaoli@163.com>
2019-08-27 17:49:09 +0800

    添加新建页面

--- app.py
+++ app.py
@@ -5,26 +5,30 @@ from db import get_db
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
+    db = get_db()
+    keyword = request.args.get('keyword', '')
+
+    # 查询数据库，按 id 倒序排列，即新添加的记录优先显示；只显示20条
+    if keyword:
+        # 如果有关键字传入则使用关键字搜索 title 字段，如：
+        # SELECT * FROM books WHERE title LIKE '%python%' LIMIT 20
+        rows = db.execute(
+            "SELECT * FROM books WHERE title LIKE '%%%s%%' ORDER BY id "
+            "LIMIT 20" % keyword)
+    else:
+        rows = db.execute("SELECT * FROM books ORDER BY id DESC LIMIT 20")
+
+    results = []
+    for row in rows:
+        results.append(row)
+    return render_template('index.html', results=results, keyword=keyword)
+
+@app.route('/book/', methods=['GET', 'POST'])
+def book():
     if request.method == 'GET':
-        db = get_db()
-        keyword = request.args.get('keyword', '')
-
-        # 查询数据库，按 id 倒序排列，即新添加的记录优先显示；只显示20条
-        if keyword:
-            # 如果有关键字传入则使用关键字搜索 title 字段，如：
-            # SELECT * FROM books WHERE title LIKE '%python%' LIMIT 20
-            rows = db.execute(
-                "SELECT * FROM books WHERE title LIKE '%%%s%%' ORDER BY id "
-                "LIMIT 20" % keyword)
-        else:
-            rows = db.execute("SELECT * FROM books ORDER BY id DESC LIMIT 20")
-
-        results = []
-        for row in rows:
-            results.append(row)
-        return render_template('index.html', results=results, keyword=keyword)
+        return render_template('book.html')
     else:
         title = request.form.get('title')
         url = request.form.get('url')
@@ -35,7 +39,7 @@ def index():
         # title，url 属于必填字段，这里进行校验；如果参数不全，则渲染提示信息
         if not title or not url:
             return render_template(
-                'index.html', form=request.form, msg=u'标题或URL不能为空')
+                'book.html', form=request.form, msg=u'标题或URL不能为空')
 
         # 校验完成后进行插入操作
         db = get_db()
